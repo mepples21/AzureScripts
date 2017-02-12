5ff671033f079b86c6ee788f6f33694ffa5f6058
Michael Epping <michael@rvrwest.com>
2017-02-12 12:06:54 -0600

    Updated the Powershell script with try/catch

--- Artifacts/Install-Exchange2010/Install-Exchange2010.ps1
+++ Artifacts/Install-Exchange2010/Install-Exchange2010.ps1
@@ -1,9 +1,25 @@
 # Install Exchange Server 2010 SP3
-Set-Location -Path C:\Install\Exchange2010SP3\
-Setup.com /r:M,C,U,HT
+try {
+    Set-Location -Path C:\Install\Exchange2010SP3\
+    Setup.com /r:M,C,U,HT
+}
+catch {
+    Write-Error "Exchagne 2010 SP3 installation failed..."
+}
 
 # Install Exchange Server 2010 SP3 Update Rollup 14
-Set-Location -Path C:\Install\
-.\Exchange2010-KB3151097-x64-en.msp /m:upgrade
+try {
+    Set-Location -Path C:\Install\
+    .\Exchange2010-KB3151097-x64-en.msp /m:upgrade
+}
+catch {
+    Write-Error "Exchange 2010 SP3 Update Rollup 14 installation failed..."
+}
 
-Restart-Computer -Force
+# Restart the machine to complete the installation
+try {
+    Restart-Computer -Force
+}
+catch {
+    Write-Error "Restart failed, please reboot manually..."
+}
